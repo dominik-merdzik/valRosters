6ddb631b6cf6750a117904a490f5a5435578a353
dominik-merdzik <92061523+dominik-merdzik@users.noreply.github.com>
2022-04-03 22:04:35 -0400

    Update index.php
    button error

--- index.php
+++ index.php
@@ -6,9 +6,6 @@
         <main class="mainIndex">   
             <div class="index-buttons-div"> 
                 <!-- buttons to take us to the different pages -->
-                <div>
-                <button onclick="location.href ='add-player.php'"class="btn btn-secondary ">Add New Player</button> 
-                </div>
                 <div> 
                 <button onclick="location.href ='list-players.php'" class="btn btn-secondary ">Show Current Players</button> 
                 </div>
